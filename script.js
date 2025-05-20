40bb7e74a7a6c0fa9a738c523f0c30d7414ed60f
Erika Lopes <erikalopespro@gmail.com>
2025-05-20 17:46:43 -0300

    apaguei variaveis no js que não estavam sendo utilizadas, criei uma nova variavel chamada resultado para poder mostrar os blocos por idade separados, adicionei novas classes e dei estilo usando css

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
-let idade = document.getElementById('idade')
-let res = document.getElementById('res')
-
 
 
 
 function verificarVacinas(){
+    let idade = document.getElementById('idade').value
+    let res = document.getElementById('res')
     let mes_ano = document.querySelector('input[name="mes_ano"]:checked')
    
     res.innerHTML = ""
 
-  if(Number(idade.value) < 0){
+  if(Number(idade) < 0 || idade.trim() === ""){
     res.innerHTML = `<h3>Digite sua idade \u{1F9D0}</h3>`
    
   } else if(!mes_ano){
     res.innerHTML = `<h3>Selecione Meses ou Anos também  \u{1F9D0} </h3>`
  
 } else{
+    let valor = Number(idade) /*pegar a idade*/
+    let tipoIdade = mes_ano.value /* pegar meses ou anos*/
+   
     
-  }
+    if(valor > 1){
+        res.innerHTML = `<h3>Vacinnas indicadas para ter se tomado até ${valor} ${tipoIdade === "mes" ? "meses" : "anos"}:</h3><ul>`;
+    } else {
+        res.innerHTML = `<h3>Vacinnas indicadas para ter se tomado até ${valor} ${tipoIdade === "mes" ? "mes" : "ano"}:</h3><ul>`;
+    }
+    
+    
+    for(let i = 0; i <= valor; i++){
+        let vacinas = vacinasPorIdade[i]
+        if (vacinas && vacinas.length > 0) {
+            let resultado = `<div class="txt-idade">${i} ${tipoIdade === "mes" ? "meses" : "anos"} </div>`
+            vacinas.forEach(vacina => {
+                resultado += `  <div class ="vacinas"> ${todasvacinas[vacina] || vacina}</div><br>`
+            });
+
+            res.innerHTML += `<div class ="vacina-bloco">${resultado}</div>`
+     }
+     }
+    }   
+    
 }
 
-let
+   
+    
+  
+
+
+
 
 /* todas as vacinas*/
 
@@ -64,6 +90,20 @@ const todasvacinas = {
 
 
 
-
+const vacinasPorIdade = {
+    0: ["bcg", "hepB"], // Recém-nascidos
+    2: ["pentavalente1", "poliomielite1", "pneumo1", "rotavirus1"], // 2 meses
+    4: ["pentavalente2", "poliomielite2", "pneumo2", "rotavirus2"], // 4 meses
+    6: ["pentavalente3", "poliomielite3", "influenza"], // 6 meses
+    12: ["tripliceViral1", "pneumoRef", "meningococicaRef"], // 12 meses
+    15: ["dTPreforco", "poliomieliteRef1", "hepA", "tetrasViral"], // 15 meses
+    4: ["dTPreforco", "poliomieliteRef2", "varicela"], // 4 a 6 anos
+    9: ["hpvMeninas", "hpvMeninos"], // 9 anos
+    11: ["meningococicaACWY", "hepB", "febreAmarela"], // 11 a 14 anos
+    18: ["trivalente", "hepB", "dTadul", "febreAmarela"], // Adultos
+    60: ["influenza", "pneumococica65", "gripe60"], // Idosos a partir de 60 anos
+    65: ["pneumococica65", "gripe60", "covid"], // Idosos maiores de 65 anos
+  };
+  
     
 
